f5d60b15d6e61a9e8f90efc68d693602d1295ab4
Eman <emanhatesschool1234@gmail.com>
2026-01-04 23:47:51 +0800

    groundwork for the contents of the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
-import ComingSoon from "./pages/ComingSoon";
+export default function App() {
 
-function App() {
-  return <ComingSoon />;
+  return (
+    <>
+      <h1>Emmanuel Bawalan</h1>
+      <p>Filipino Full Stack Developer and Third Year Information Technology Student</p>
+      <button>LinkedIn</button>
+      <button>GitHub</button>
+      <button>Facebook</button>
+      <h1>Projects</h1>
+      <h2>IT Archives</h2>
+      <p>Learning Resources Archive for IT related topics</p>
+      <ul>
+        <li>React</li>
+        <li>TypeScript</li>
+        <li>3</li>
+      </ul>
+      <h2>Burger Simulator</h2>
+      <p>Progressive point-and-click simulator game</p>
+      <ul>
+        <li>Pygame</li>
+        <li>Python</li>
+        <li>3</li>
+      </ul>
+      <h1>Education</h1>
+      <h2>Bachelor of Science in Information Technology with specialization in Mobile and Web Applications</h2>
+      <p>2023 - present</p>
+      <p>National University - Dasmarinas</p>
+      <h1>Tech Stack</h1>
+      <h2>Specializes in</h2>
+      <ul>
+        <li>MongoDB</li>
+        <li>ExpressJS</li>
+        <li>React</li>
+        <li>Node.JS</li>
+        <li>Tailwind</li>
+      </ul>
+      <h2>Have experiences in</h2>
+      <ul>
+        <li>Django</li>
+        <li>Flutter</li>
+        <li>Php</li>
+        <li>Java</li>
+        <li>Bootstrap</li>
+      </ul>
+    </>
+  );
 }
-
-export default App;
